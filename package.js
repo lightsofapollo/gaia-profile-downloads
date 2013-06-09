8af7e8fb62d5d604166421ca6b0e89dca59564cc
James Lal <james@lightsofapollo.com>
2013-06-09 16:05:22 -0700

    npmignore + move lib/ to build/

--- package.js
+++ package.js
@@ -1,4 +1,4 @@
-var git = require('./lib/git')(),
+var git = require('./build/git')(),
     fs = require('fs'),
     http = require('http'),
     remove = require('remove'),
@@ -33,7 +33,7 @@ function createProfile(gaia, branch, callback) {
 
 var EXCLUDES = ['HEAD'];
 function findBranches() {
-  var gaia = new (require('./lib/gaia').Gaia)(GAIA);
+  var gaia = new (require('./build/gaia').Gaia)(GAIA);
   gaia.branches(function(err, list) {
     list = list.filter(function(item) {
       if (EXCLUDES.indexOf(item) === -1)
